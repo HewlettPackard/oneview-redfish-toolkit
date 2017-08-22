aa0455b3135fd16219aff546e13bd014b91d407a
Flávio Fonseca <ff@darkpenguin.com.br>
2017-08-22 15:38:08 -0300

    Added util.py with load_config and load_schemas function. Added schemas dirs wil all json schemas from DPS8010_2017.1.zip files. Added unit tests for both functions

---
+++ oneview_redfish_toolkit/tests/test_util_load_config.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+    Tests for load_config function from util.py
+"""
+
+import configparser
+import unittest
+
+from oneview_redfish_toolkit import util
+
+
+class TestUtilLoadConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.config_file = './oneview_redfish_toolkit/redfish.ini'
+
+    def test_invalid_config_file(self):
+        # Tests if passing a file that does not exists returns false.
+        self.assertFalse(util.load_config('non-exist.ini'))
+
+    def test_valid_config_file(self):
+        # Tests if passing a valid file returns a object
+
+        self.assertIsInstance(util.load_config(self.config_file),
+                              configparser.ConfigParser)
+
+    def test_has_all_expect_sessions(self):
+        # Tests if ini file has all expected sections
+
+        cfg = util.load_config(self.config_file)
+
+        self.assertTrue(cfg.has_section('directories'),
+                        msg='Section {} not found in ini file {}'.format(
+                        'directories', self.config_file))
+        self.assertTrue(cfg.has_section('oneview_config'),
+                        msg='Section {} not found in ini file {}'.
+                        format('oneview_config', self.config_file))
+        self.assertTrue(cfg.has_section('credentials'),
+                        msg='Section {} not found in ini file {}'.
+                        format('credentials', self.config_file))
+        self.assertTrue(cfg.has_section('schemas'),
+                        msg='Section {} not found in ini file {}'.
+                        format('schemas', self.config_file))
+
+    def test_has_all_options(self):
+        # Tests if ini file has all expected options
+
+        cfg = util.load_config(self.config_file)
+
+        self.assertTrue(cfg.has_option('directories', 'schema_dir'),
+                        msg='Option {} not found in section {} in ini file {}'
+                        .format('schema_dir', 'directories',
+                        self.config_file))
+        self.assertTrue(cfg.has_option('oneview_config', 'ip'),
+                        msg='Option {} not found in section {} in ini file {}'
+                        .format('ip', 'oneview_config', self.config_file))
+        self.assertTrue(cfg.has_option('oneview_config', 'api_version'),
+                        msg='Option {} not found in section {} in ini file {}'
+                        .format('schema_dir', 'oneview_config',
+                        self.config_file))
+        self.assertTrue(cfg.has_option('credentials', 'userName'),
+                        msg='Option {} not found in section {} in ini file {}'
+                        .format('userName', 'credentials',
+                        self.config_file))
+        self.assertTrue(cfg.has_option('credentials', 'password'),
+                        msg='Option {} not found in section {} in ini file {}'
+                        .format('password', 'credentials',
+                        self.config_file))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ oneview_redfish_toolkit/util.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import os
+
+import collections
+import configparser
+import json
+
+
+def load_config(ini_file):
+    """Loads ini file
+
+        Loads and parser the system ini file. The default file is redfish.ini.
+        Ini file keys are keep their cases.
+
+        Args:
+            ini_file: string with the ini file name
+
+        Returns:
+            If fails: print a error message and returns False
+            If succeed: ConfigParser object with init file contents
+    """
+
+    if os.path.isfile(ini_file) is False:
+        print("Ini file {} not found".format(ini_file))
+        return False
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    try:
+        config.read(ini_file)
+    except Exception as e:
+        print(e)
+        return False
+    return config
+
+
+def load_schemas(schema_dir, schemas):
+    """Loads schemas
+
+        Loads all schemas in listed in schemas searching on schema_dir
+        directory
+
+        Args:
+            schema_dir: string with the directory to load schemas from
+            schemas: dict with schema name as key and schema file_name
+                as value. The key will also be the key in the returning dict
+
+        Returns:
+            If fails: print a error message and returns False
+            If succeed: OrderedDict: A dict containing
+                ('SchemasName' : schema_obj) pairs
+    """
+
+    if os.path.isdir(schema_dir) is False:
+        print("Schema dir is not a valid dir: {}".format(schema_dir))
+        return False
+    if os.access(schema_dir, os.R_OK) is False:
+        print("Can't access dir {}".format(schema_dir))
+        return False
+
+    schema_dict = collections.OrderedDict()
+    for key in schemas:
+        try:
+            with open(schema_dir + '/' + schemas[key]) as F:
+                schema_dict[key] = json.load(F)
+        except Exception as e:
+            print(e)
+            return False
+    return schema_dict
+
+
+if __name__ == '__main__':
+    cfg = load_config('redfish.ini')
+    schemas = dict(cfg.items('schemas'))
+    schemas_dict = load_schemas(cfg['directories']['schema_dir'], schemas)
+    print(schemas_dict)
