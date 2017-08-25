0169660b3a4671e6eb5ee904308c3f655ed5b47a
Lucas Vieira <lima.lucaslv@gmail.com>
2017-08-24 23:50:04 -0300

    Adding Computer System Collection Blueprint   * Create computer_system_collection.py blueprint and your respective unit test

---
+++ oneview_redfish_toolkit/blueprints/computer_system_collection.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from flask import abort
+from flask import Blueprint
+from flask import current_app
+from flask import jsonify
+from flask import make_response
+from flask import Response
+from flask_api import status
+
+from hpOneView.exceptions import HPOneViewException
+
+from oneview_redfish_toolkit.api.computer_system_collection \
+    import ComputerSystemCollection
+
+computer_system_collection_root = Blueprint(
+    "computer_system_collection_root", __name__)
+
+
+@computer_system_collection_root.route("/", methods=["GET"])
+def get_computer_system_collection():
+    """Get the Redfish Computer System Collection.
+
+        Get method to return ComputerSystemCollection JSON when
+        /redfish/v1/Systems is requested.
+
+        Returns:
+                JSON: JSON with ComputerSystemCollection.
+    """
+    try:
+        oneview_computer_system_collection = \
+            current_app.oneview_client.server_hardware.get_all()
+    except HPOneViewException:
+        return abort(status.HTTP_404_NOT_FOUND)
+
+    obj = ComputerSystemCollection(
+        current_app.schemas_dict["ComputerSystemCollection"],
+        oneview_computer_system_collection)
+
+    json_str = obj.serialize(True)
+    response = Response(
+        response=json_str,
+        status=status.HTTP_200_OK,
+        mimetype="application/json")
+
+    return response
+
+
+@computer_system_collection_root.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found_computer_system_collection(error):
+    """Improve not found error message.
+
+        Show a JSON with not found error message
+        to Computer system collection.
+
+        Returns:
+                JSON: error message.
+    """
+    return make_response(jsonify(
+        {"error": "Computer System Collection not found."}),
+        status.HTTP_404_NOT_FOUND)
