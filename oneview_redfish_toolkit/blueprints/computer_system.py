d9a2a014fed1e49f61c1a23316cc416f245248e1
Flávio Fonseca <ff@darkpenguin.com.br>
2017-09-04 11:26:16 -0300

    Add ComputerSystem

---
+++ oneview_redfish_toolkit/blueprints/computer_system.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Python libs
+import logging
+
+# 3rd party libs
+from flask import abort
+from flask import Blueprint
+from flask import Response
+from flask_api import status
+
+# own libs
+from hpOneView.exceptions import HPOneViewException
+from oneview_redfish_toolkit.api.computer_system import ComputerSystem
+from oneview_redfish_toolkit import util
+
+
+computer_system = Blueprint("computer_system", __name__)
+
+
+@computer_system.route("/redfish/v1/Systems/<uuid>", methods=["GET"])
+def get_computer_system(uuid):
+    """Get the Redfish Computer System for a given UUDI.
+
+        Return ComputerSystem redfish JSON for a given
+        server harder UUID.
+        Logs exception of any error and return abort(500)
+        Internal Server Error.
+
+        Returns:
+            JSON: Redfish json with ComputerSystem
+            When Server hardware is not found calls abort(404)
+
+
+        Exceptions:
+            Logs the exception and call abort(500)
+
+    """
+    try:
+        # Recover OV connection
+        ov_client = util.get_oneview_client()
+
+        # Gets serverhardware for given UUID
+        sh = ov_client.server_hardware.get(uuid)
+
+        # Gets de server hardware type of the given server hardware
+        sht = ov_client.server_hardware_types.get(sh['serverHardwareTypeUri'])
+
+        # Build Computer System object and validates it
+        cs = ComputerSystem(sh, sht)
+
+        # Build redfish json
+        json_str = cs.serialize()
+
+        # Build response and returns
+        return Response(
+            response=json_str,
+            status=status.HTTP_200_OK,
+            mimetype="application/json")
+    except HPOneViewException as e:
+        if e.error_code == "RESOURCE_NOT_FOUND":
+            logging.warning('ServerHardware UUID {} not found'.format(uuid))
+            abort(status.HTTP_404_NOT_FOUND)
+        elif e.message.find("server-hardware-types"):
+            logging.error(
+                'OneView Exception while looking for server hardware type'
+                ' {}'.format(e)
+            )
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif e.message.find("server-hardware"):
+            logging.error(
+                'OneView Exception while looking for '
+                'server hardware: {}'.format(e)
+            )
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            logging.error('Unexpected OneView Exception: {}'.format(e))
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except Exception as e:
+        # In case of error print exception and abort
+        logging.error('Unexepected error: '.format(e))
+        return abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@computer_system.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """Creates a Not Found Error response"""
+    return Response(
+        response={'error': 'URL not found'},
+        status=status.HTTP_404_NOT_FOUND,
+        mimetype='application/json')
+
+
+@computer_system.errorhandler(
+    status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """Creates an Internal Server Error response"""
+    logging.error(vars(error))
+    return Response(
+        response={'error': 'Internal Server Error'},
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        mimetype='application/json')
---
+++ oneview_redfish_toolkit/tests/blueprints/test_computer_system.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Python libs
+import json
+import unittest
+from unittest import mock
+
+# 3rd party libs
+from flask import Flask
+from flask_api import status
+from hpOneView.exceptions import HPOneViewException
+from oneview_redfish_toolkit import util
+
+# Module libs
+from oneview_redfish_toolkit.blueprints.computer_system import computer_system
+
+
+class TestComputerSystem(unittest.TestCase):
+    """Tests for ComputerSystem blueprint
+
+        Tests:
+            - server hardware not found
+            - oneview exception
+            - know computer system
+    """
+
+    @mock.patch.object(util, 'OneViewClient')
+    def setUp(self, ov_mock):
+        """Tests preparation"""
+
+        # Load config on util
+        util.load_config('oneview_redfish_toolkit/redfish.ini')
+
+        # creates a test client
+        self.app = Flask(__name__)
+
+        self.app.register_blueprint(computer_system)
+
+        self.app = self.app.test_client()
+
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    @mock.patch.object(util, 'get_oneview_client')
+    def test_get_computer_system_not_found(self, mock_get_ov_client):
+        """Tests ComputerSystem with an empty list"""
+
+        client = mock_get_ov_client()
+        e = HPOneViewException("ServerHardware not found")
+        e.error_code = "RESOURCE_NOT_FOUND"""
+        client.server_hardware.get.side_effect = e
+
+        response = self.app.get(
+            "/redfish/v1/Systems/0303437-3034-4D32-3230-313133364752"
+        )
+
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+        self.assertEqual("application/json", response.mimetype)
+
+    @mock.patch.object(util, 'get_oneview_client')
+    def test_get_computer_system_fail(self, mock_get_ov_client):
+        """Tests ComputerSystem with an error"""
+
+        client = mock_get_ov_client()
+        client.server_hardware.get.side_effect = Exception()
+
+        response = self.app.get(
+            "/redfish/v1/Systems/0303437-3034-4D32-3230-313133364752"
+        )
+
+        self.assertEqual(
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            response.status_code)
+        self.assertEqual("application/json", response.mimetype)
+
+    @mock.patch.object(util, 'get_oneview_client')
+    def test_get_computer_system(self, mock_get_ov_client):
+        """Tests ComputerSystem with a known Server Hardware list"""
+
+        # Loading server_hardware mockup value
+        with open(
+            'oneview_redfish_toolkit/mockups/ServerHardware.json'
+        ) as f:
+            sh_dict = json.load(f)
+
+        # Loading ServerHardwareTypes mockup value
+        with open(
+            'oneview_redfish_toolkit/mockups/ServerHardwareTypes.json'
+        ) as f:
+            sht_dict = json.load(f)
+
+        # Loading ComputerSystem mockup result
+        with open(
+            'oneview_redfish_toolkit/mockups/ComputerSystem.json'
+        ) as f:
+            computer_system = f.read()
+
+        # Create mock response
+        ov = mock_get_ov_client()
+        ov.server_hardware.get.return_value = sh_dict
+        ov.server_hardware_types.get.return_value = sht_dict
+
+        # Get ComputerSystem
+        response = self.app.get(
+            "/redfish/v1/Systems/0303437-3034-4D32-3230-313133364752"
+        )
+
+        # Gets json from response
+        json_str = response.data.decode("utf-8")
+
+        # Tests response
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual("application/json", response.mimetype)
+        self.assertEqual(computer_system, json_str)
