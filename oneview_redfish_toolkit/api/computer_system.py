c374189d54e1b1e1f29adb1536c38cee1b2be411
Flávio Fonseca <ff@darkpenguin.com.br>
2017-10-10 12:22:44 -0300

    Change EthernetInterfaces to NetworkInterfaces

--- oneview_redfish_toolkit/api/computer_system.py
+++ oneview_redfish_toolkit/api/computer_system.py
@@ -99,11 +99,11 @@ class ComputerSystem(RedfishJsonValidator):
         self.redfish["Storage"] = collections.OrderedDict()
         self.redfish["Storage"]["@odata.id"] = \
             "/redfish/v1/Systems/" + server_hardware['uuid'] + "/Storage"
-        self.redfish["EthernetInterfaces"] = collections.OrderedDict()
-        self.redfish["EthernetInterfaces"]["@odata.id"] = \
+        self.redfish["NetworkInterfaces"] = collections.OrderedDict()
+        self.redfish["NetworkInterfaces"]["@odata.id"] = \
             "/redfish/v1/Systems/" + \
             server_hardware['uuid'] + \
-            "/EthernetInterfaces"
+            "/NetworkInterfaces"
         self.redfish["Links"] = collections.OrderedDict()
         self.redfish["Links"]["Chassis"] = list()
         self.redfish["Links"]["Chassis"].append(collections.OrderedDict())
