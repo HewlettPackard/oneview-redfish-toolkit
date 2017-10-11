2c8c34e5dce5da5902ea729ba57344aa91c2ad51
ffdarkpenguin <ff@darkpenguin.com.br>
2017-10-11 12:06:03 -0300

    Add network interface collection#119 (#126)

---
+++ oneview_redfish_toolkit/api/network_interface_collection.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oneview_redfish_toolkit.api.redfish_json_validator \
+    import RedfishJsonValidator
+
+
+class NetworkInterfaceCollection(RedfishJsonValidator):
+    """Creates a Network Interface Collection Redfish dict
+
+        Populates self.redfish with some hardcoded NetworkInterfaceCollection
+        values and Oneview ServerHardware result
+    """
+
+    SCHEMA_NAME = 'NetworkInterfaceCollection'
+
+    def __init__(self, server_hardware):
+        """NetworkInterfaceCollection constructor
+
+            Populates self.redfish and calls _validate()
+
+            Args:
+                server_hardware: a server hardware dict from OneView
+        """
+        super().__init__(self.SCHEMA_NAME)
+        self.redfish["@odata.type"] = \
+            "#NetworkInterfaceCollection.NetworkInterfaceCollection"
+        self.redfish["Name"] = "Network Interface Collection"
+        members_count = 0
+        self.redfish["Members"] = list()
+        for device in server_hardware["portMap"]["deviceSlots"]:
+            for port in device["physicalPorts"]:
+                if port["type"] in ["Ethernet", "FibreChannel", "InfiniBand"]:
+                    members_count += 1
+                    device_link = dict()
+                    device_link["@odata.id"] = \
+                        "/redfish/v1/Systems/" + server_hardware["uuid"] + \
+                        "/NetworkInterface/" + str(device["deviceNumber"])
+                    self.redfish["Members"].append(device_link)
+                    break
+        self.redfish["Members@odata.count"] = members_count
+        self.redfish["@odata.context"] = \
+            "/redfish/v1/$metadata#NetworkInterfaceCollection" \
+            ".NetworkInterfaceCollection"
+        self.redfish["@odata.id"] = "/redfish/v1/Systems/" + \
+            server_hardware["uuid"] + "/NetworkInterfaces"
+
+        self._validate()
---
+++ oneview_redfish_toolkit/blueprints/network_interface_collection.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2017) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from flask import abort
+from flask import Blueprint
+from flask import Response
+from flask_api import status
+
+from hpOneView.exceptions import HPOneViewException
+from oneview_redfish_toolkit.api.network_interface_collection \
+    import NetworkInterfaceCollection
+
+from oneview_redfish_toolkit import util
+
+import logging
+
+network_interface_collection = Blueprint(
+    "network_interface_collection", __name__)
+
+
+@network_interface_collection.route(
+    "/redfish/v1/Systems/<uuid>/NetworkInterfaces", methods=["GET"])
+def get_network_interface_collection(uuid):
+    """Get the Redfish Network Interfaces Collection.
+
+    Return NetworkInterfaceCollection Redfish JSON.
+    """
+    try:
+        oneview_client = util.get_oneview_client()
+
+        server_hardware = oneview_client.server_hardware.get(uuid)
+
+        nic = NetworkInterfaceCollection(server_hardware)
+
+        json_str = nic.serialize()
+
+        return Response(
+            response=json_str,
+            status=status.HTTP_200_OK,
+            mimetype="application/json")
+
+    except HPOneViewException as e:
+        if e.oneview_response['errorCode'] == "RESOURCE_NOT_FOUND":
+            logging.warning('Server hardware UUID {} not found'.format(uuid))
+            abort(status.HTTP_404_NOT_FOUND, "Server hardware not found")
+        elif e.msg.find("server-hardware") >= 0:
+            logging.error(
+                'OneView Exception while looking for '
+                'server hardware: {}'.format(e)
+            )
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            logging.error('Unexpected OneView Exception: {}'.format(e))
+            abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except Exception as e:
+        # In case of error print exception and abort
+        logging.error('Unexpected error: '.format(e))
+        return abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
