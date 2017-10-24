273b5f7d453ac97f712643b5a040aab47c4417c7
Lucas Lima Vieira <lima.lucaslv@gmail.com>
2017-10-24 10:55:03 -0200

    Fix device_id range validation (#137)

--- oneview_redfish_toolkit/blueprints/network_interface.py
+++ oneview_redfish_toolkit/blueprints/network_interface.py
@@ -63,8 +63,8 @@ def get_network_interface(uuid, device_id):
 
         server_hardware = oneview_client.server_hardware.get(uuid)
 
-        if device_id_validation < 0 or (device_id_validation - 1) >= \
-            len(server_hardware["portMap"]["deviceSlots"]):
+        if (device_id_validation - 1) < 0 or (device_id_validation - 1) >= \
+                len(server_hardware["portMap"]["deviceSlots"]):
             raise OneViewRedfishResourceNotFoundError(
                 device_id, "Network interface")
 
