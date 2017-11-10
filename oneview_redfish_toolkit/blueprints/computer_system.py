4fa8cde54e2fa2a1b4ca8d733ce111698543c22b
Flávio Fonseca <ff@darkpenguin.com.br>
2017-11-10 11:00:40 -0200

    Fix logging call bug in blueprints

--- oneview_redfish_toolkit/blueprints/computer_system.py
+++ oneview_redfish_toolkit/blueprints/computer_system.py
@@ -107,7 +107,7 @@ def get_computer_system(uuid):
             abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
     except Exception as e:
         # In case of error print exception and abort
-        logging.error('Unexpected error: '.format(e))
+        logging.error('Unexpected error: {}'.format(e))
         return abort(status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
