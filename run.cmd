99b16a035defeb85eb7b84e589d38ecfccc9ddd5
ffdarkpenguin <ff@darkpenguin.com.br>
2017-11-28 12:25:10 -0200

    Add windows start script, run.cmd (#182)

---
+++ run.cmd
@@ -0,0 +1,4 @@
+@echo This script assumes that the python executables needed by this system is reacheble via PATH environment variable
+@echo It also assumes that all required libs are installed and if necessary the virtualenv to run the server is activated
+
+python oneview_redfish_toolkit/app.py
