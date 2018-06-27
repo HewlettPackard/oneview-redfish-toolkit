6af1cdff2e0801789a7a643604f5ff5bf1a89587
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2018-06-27 14:12:16 +0000

    Adds endpoints to Drives and Storage details of ResourceBlock
    Includes:
    - StorageDriveCompositionDetails to represent a redfish object of Drives of a Storage
    - StorageCompositionDetails to represent a redfish object of Storage of a Storage ResourceBlock
    - Endpoints to show that objects
    - Unit tests to that objects and endpoints
    - Adds some Mockups

---
+++ oneview_redfish_toolkit/api/storage_composition_details.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2018) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oneview_redfish_toolkit.api.redfish_json_validator import \
+    RedfishJsonValidator
+from oneview_redfish_toolkit.api.resource_block_collection import \
+    ResourceBlockCollection
+from oneview_redfish_toolkit.api import status_mapping
+
+
+class StorageCompositionDetails(RedfishJsonValidator):
+    """Creates a StorageCompositionDetails dict
+
+            Populates self.redfish with data retrieved from
+            an OneView's Drive
+    """
+
+    SCHEMA_NAME = 'Storage'
+
+    def __init__(self, drive):
+        """StorageCompositionDetails constructor
+
+            Populates self.redfish with the contents of devices from an
+            Oneview's Drive
+
+            Args:
+                drive: Oneview's Drive dict
+        """
+        super().__init__(self.SCHEMA_NAME)
+
+        drive_uuid = drive["uri"].split("/")[-1]
+        self.odata_id = "{}/{}/Storage/1"\
+            .format(ResourceBlockCollection.BASE_URI, drive_uuid)
+
+        self.redfish["@odata.type"] = "#Storage.v1_2_0.Storage"
+        self.redfish["Id"] = "1"
+        self.redfish["Name"] = drive["name"]
+        self.redfish["Status"] = status_mapping.STATUS_MAP.get(drive["status"])
+        self.redfish["Drives"] = [
+            {
+                "@odata.id": self.odata_id + "/Drives/1"
+            }
+        ]
+        self.redfish["@odata.context"] = \
+            "/redfish/v1/$metadata#Storage.Storage"
+
+        self.redfish["@odata.id"] = self.odata_id
+
+        self._validate()
---
+++ oneview_redfish_toolkit/tests/api/test_storage_composition_details.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (2018) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+from oneview_redfish_toolkit.api.storage_composition_details import \
+    StorageCompositionDetails
+from oneview_redfish_toolkit.tests.base_test import BaseTest
+
+
+class TestStorageCompositionDetails(BaseTest):
+    """Tests for StorageCompositionDetails class"""
+
+    def setUp(self):
+        """Tests preparation"""
+
+        with open(
+            'oneview_redfish_toolkit/mockups/oneview/Drive.json'
+        ) as f:
+            self.drive = json.load(f)
+
+    def test_serialize(self):
+        with open(
+            'oneview_redfish_toolkit/mockups/redfish/'
+            'StorageCompositionDetails.json'
+        ) as f:
+            expected_result = json.load(f)
+
+        target = StorageCompositionDetails(self.drive)
+
+        result = json.loads(target.serialize())
+
+        self.assertEqual(expected_result, result)
