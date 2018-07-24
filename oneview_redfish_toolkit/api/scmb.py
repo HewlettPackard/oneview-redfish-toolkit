0df05a441b1fe9c1b0276d0071cb98574cc444f1
Adriane Cardozo <adriane.cardozo@gmail.com>
2018-07-24 13:39:44 -0700

    workaround to use API version 500 for 'conf' authentication mode

--- oneview_redfish_toolkit/api/scmb.py
+++ oneview_redfish_toolkit/api/scmb.py
@@ -20,16 +20,18 @@ import os
 import pika
 import ssl
 
+from copy import deepcopy
 from hpOneView.exceptions import HPOneViewException
+from hpOneView.oneview_client import OneViewClient
 from pika.credentials import ExternalCredentials
 
 from oneview_redfish_toolkit.api.event import Event
 from oneview_redfish_toolkit import util
 
 
-ONEVIEW_CA = "certs/oneview_ca.pem"
-SCMB_CERT = "certs/oneview_scmb.pem"
-SCMB_KEY = "certs/oneview_scmb.key"
+ONEVIEW_CA = "oneview_redfish_toolkit/certs/oneview_ca.pem"
+SCMB_CERT = "oneview_redfish_toolkit/certs/oneview_scmb.pem"
+SCMB_KEY = "oneview_redfish_toolkit/certs/oneview_scmb.key"
 SCMB_PORT = 5671
 SCMB_SOCKET_TIMEOUT = 5  # seconds
 SCMB_RESOURCE_LIST = [
@@ -47,7 +49,18 @@ def check_cert_exist():
 
 def get_cert():
     # Get CA
-    ov_client = util.get_oneview_client()
+
+    # Workaround for #328
+    # Create OneView client using API 500 just to retrieve OneView certificates
+    try:
+        ov_config = deepcopy(util.ov_config)
+        ov_config['api_version'] = 500
+        ov_client = OneViewClient(ov_config)
+        ov_client.connection.login(ov_config['credentials'])
+    # if failed abort
+    except Exception:
+        raise
+
     cert = ov_client.certificate_authority.get()
 
     with open(ONEVIEW_CA, 'w+') as f:
