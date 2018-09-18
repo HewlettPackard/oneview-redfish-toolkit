2d4edb0835f841232e14b65d54d40c2c0a92702d
Victor Hugo R. Rodrigues <victor.hugorr@gmail.com>
2018-09-18 14:15:08 -0300

    Concurrent performance tests

--- oneview_redfish_toolkit/tests/_parallel.py
+++ oneview_redfish_toolkit/tests/_parallel.py
@@ -1,25 +1,42 @@
+#!/usr/bin/python3
 import json
 import logging
 import logging.config
 import ssl
 from threading import Thread
+from random import randint
 import time
 import urllib3
 
 urllib3.disable_warnings()
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+logging.basicConfig(filename='results.log', level=logging.INFO)
 
-REDFISH_IP = 'redfish-server.net'
+REDFISH_IP = 'blso-redfish-server-01.vse.rdlabs.hpecorp.net'
+#REQ_URL = '/redfish/'
 REQ_URL = '/redfish/v1/CompositionService/ResourceBlocks/197a62e4-f2c7-4097-a098-9ade31a4a353/Storage/1'
+#REQ_URL = '/redfish/v1/CompositionService/ResourceBlocks/197a62e4-f2c7-4097-a098-9ade31a4a353'
+#REQ_URL = ['/redfish/v1/CompositionService/ResourceZones/13d6739e-2856-4744-8a10-63ccbda0268e-0000000000A66101',
+#'/redfish/v1/CompositionService/ResourceBlocks/197a62e4-f2c7-4097-a098-9ade31a4a353',
+#'/redfish/v1/CompositionService/ResourceBlocks/197a62e4-f2c7-4097-a098-9ade31a4a353/Storage/1',
+#'/redfish/v1/CompositionService/ResourceBlocks/30303437-3034-4D32-3230-313130304752',
+#'/redfish/v1/CompositionService/ResourceBlocks/30303437-3034-4D32-3230-313130304752/Systems/1',
+#'/redfish/v1/CompositionService/ResourceBlocks/30303437-3034-4D32-3230-313130304752/Systems/1/Processors',
+#'/redfish/v1/CompositionService/ResourceBlocks/30303437-3034-4D32-3230-313130304752/Systems/1/Processors/1',
+#'/redfish/v1/CompositionService/ResourceBlocks/13d6739e-2856-4744-8a10-63ccbda0268e',
+#'/redfish/v1/CompositionService/ResourceBlocks/13d6739e-2856-4744-8a10-63ccbda0268e/EthernetInterfaces/1']
 
-http = urllib3.HTTPSConnectionPool(REDFISH_IP, 5000, cert_reqs='CERT_NONE')
+N_REQ_PER_THREAD = 100
+
+#http = urllib3.HTTPSConnectionPool(REDFISH_IP, 5000, cert_reqs='CERT_NONE')
 
 def function_to_run():
-#    http = urllib3.HTTPSConnectionPool(REDFISH_IP, 5000, cert_reqs='CERT_NONE')
-
-    resp = http.request('GET', REQ_URL)
-
-#    if resp.status != 200:
-#        print(resp.data)
+    http = urllib3.HTTPSConnectionPool(REDFISH_IP, 5000, cert_reqs='CERT_NONE')
+    for i in range(N_REQ_PER_THREAD):
+        resp = http.request('GET', REQ_URL)
+        #resp = http.request('GET', REQ_URL[randint(0,8)])
+        if resp.status != 200:
+            logging.info(resp.data)
 
     pass
 
@@ -32,26 +49,28 @@ def threaded(num_threads):
     funcs = []
     for i in range(int(num_threads)):
         funcs.append(threads_object())
+    
     for i in funcs:
         i.start()
+    
     for i in funcs:
         i.join()
+
 
 def show_results(func_name, results):
     msg = "{} {} seconds".format(func_name, results)
     print(msg)
 
+
 if __name__ == '__main__':
+    logging.info('Starting tests')
 
-    repeat_list = [1, 3, 4, 5, 6, 7]
+    for n_threads in range(2, 150, 5):
+        start_time = time.time()
+        threaded(n_threads)
+        end_time = time.time()
+        total_time = (end_time - start_time) / N_REQ_PER_THREAD
 
-    print('Starting tests')
+        logging.info("Threads {} : {}".format(n_threads, total_time))
 
-    for repeat in range(2, 150, 2):
-        start_thread = time.time()
-        threaded(repeat)
-        print("Threads {} : {}".format(repeat, (time.time() - start_thread)))
-        time.sleep(3)
-
-
-    print('Iterations complete')
+    logging.info('Iterations complete')
