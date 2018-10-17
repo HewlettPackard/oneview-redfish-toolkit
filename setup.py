a9815fd9a94f2f72818ed04d96aa3f238ca28b72
Victor Hugo R. Rodrigues <victor.hugorr@gmail.com>
2018-10-17 11:19:39 -0300

    Removing specific version for PBR

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     scripts=['scripts/oneview-redfish-toolkit'],
     setup_requires=["pbr"],
-    download_url="https://github.com/HewlettPackard/oneview-redfish-toolkit/tarball/0.3.0",
+    download_url="https://github.com/HewlettPackard/oneview-redfish-toolkit/"
+    "tarball/0.3.0",
     python_requires='>=3.5',
     pbr=True
     )
