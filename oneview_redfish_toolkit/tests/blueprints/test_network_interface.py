3ead0389acede2fd7f4cab8e7a65215860434cdb
bksaroja <saroja.2428@gmail.com>
2020-10-31 12:58:20 -0500

    fixed test cases in test_network_interface.py

--- oneview_redfish_toolkit/tests/blueprints/test_network_interface.py
+++ oneview_redfish_toolkit/tests/blueprints/test_network_interface.py
@@ -20,6 +20,8 @@ import json
 # 3rd party libs
 from flask_api import status
 from hpOneView.exceptions import HPOneViewException
+from hpOneView.resources.servers.server_profiles import ServerProfiles
+from hpOneView.resources.servers.server_hardware import ServerHardware
 
 # Module libs
 from oneview_redfish_toolkit.blueprints import network_interface
@@ -58,12 +60,12 @@ class TestNetworkInterface(BaseFlaskTest):
             'NetworkInterface3.json'
         ) as f:
             network_interface_mockup = json.load(f)
-
-        self.oneview_client.server_profiles.get.return_value = \
-            self.server_profile
-
-        self.oneview_client.server_hardware.get.return_value = \
-            self.server_hardware
+        profile_obj = ServerProfiles(self.oneview_client, self.server_profile)
+        self.oneview_client.server_profiles.get_by_id.return_value = \
+            profile_obj
+        serverhw_obj = ServerHardware(self.oneview_client, self.server_hardware)
+        self.oneview_client.server_hardware.get_by_uri.return_value = \
+            serverhw_obj
 
         response = self.client.get(
             "/redfish/v1/Systems/b425802b-a6a5-4941-8885-aab68dfa2ee2/"
@@ -75,18 +77,19 @@ class TestNetworkInterface(BaseFlaskTest):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual("application/json", response.mimetype)
         self.assertEqualMockup(network_interface_mockup, result)
-        self.oneview_client.server_profiles.get.assert_called_with(
+        self.oneview_client.server_profiles.get_by_id.assert_called_with(
             self.server_profile["uuid"])
-        self.oneview_client.server_hardware.get.assert_called_with(
+        self.oneview_client.server_hardware.get_by_uri.assert_called_with(
             self.server_profile["serverHardwareUri"])
 
     def test_get_network_interface_invalid_id(self):
         """Tests NetworkInterfaceCollection"""
-
+        profile_obj = ServerProfiles(self.oneview_client, self.server_profile)
+        serverhw_obj = ServerHardware(self.oneview_client, self.server_hardware)
         self.oneview_client.\
-            server_profiles.get.return_value = self.server_profile
-        self.oneview_client.server_hardware.get.return_value = \
-            self.server_hardware
+            server_profiles.get_by_id.return_value = profile_obj
+        self.oneview_client.server_hardware.get_by_uri.return_value = \
+            serverhw_obj
 
         response = self.client.get(
             "/redfish/v1/Systems/b425802b-a6a5-4941-8885-aab68dfa2ee2/"
@@ -95,15 +98,15 @@ class TestNetworkInterface(BaseFlaskTest):
 
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
         self.assertEqual("application/json", response.mimetype)
-        self.oneview_client.server_profiles.get.assert_not_called()
-        self.oneview_client.server_hardware.get.assert_not_called()
+        self.oneview_client.server_profiles.get_by_id.assert_not_called()
+        self.oneview_client.server_hardware.get_by_uri.assert_not_called()
 
     def test_get_network_interface_server_profile_not_found(
             self):
         """Tests NetworkInterface server profile not found"""
 
         self.oneview_client.\
-            server_profiles.get.side_effect = self.not_found_error
+            server_profiles.get_by_id.side_effect = self.not_found_error
 
         response = self.client.get(
             "/redfish/v1/Systems/b425802b-a6a5-4941-8885-aab68dfa2ee2/"
@@ -112,18 +115,18 @@ class TestNetworkInterface(BaseFlaskTest):
 
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
         self.assertEqual("application/json", response.mimetype)
-        self.oneview_client.server_profiles.get.assert_called_with(
+        self.oneview_client.server_profiles.get_by_id.assert_called_with(
             self.server_profile["uuid"])
-        self.oneview_client.server_hardware.get.assert_not_called()
+        self.oneview_client.server_hardware.get_by_uri.assert_not_called()
 
     def test_get_network_interface_sh_not_found(
         self):
         """Tests NetworkInterface server hardware not found"""
-
+        profile_obj = ServerProfiles(self.oneview_client, self.server_profile)
         self.oneview_client.\
-            server_profiles.get.return_value = self.server_profile
+            server_profiles.get_by_id.return_value = profile_obj
         self.oneview_client.\
-            server_hardware.get.side_effect = self.not_found_error
+            server_hardware.get_by_uri.side_effect = self.not_found_error
 
         response = self.client.get(
             "/redfish/v1/Systems/b425802b-a6a5-4941-8885-aab68dfa2ee2/"
@@ -132,9 +135,9 @@ class TestNetworkInterface(BaseFlaskTest):
 
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
         self.assertEqual("application/json", response.mimetype)
-        self.oneview_client.server_profiles.get.assert_called_with(
+        self.oneview_client.server_profiles.get_by_id.assert_called_with(
             self.server_profile["uuid"])
-        self.oneview_client.server_hardware.get.assert_called_with(
+        self.oneview_client.server_hardware.get_by_uri.assert_called_with(
             self.server_profile["serverHardwareUri"])
 
     def test_get_network_interface_server_profile_exception(
@@ -145,7 +148,7 @@ class TestNetworkInterface(BaseFlaskTest):
             'errorCode': 'ANOTHER_ERROR',
             'message': 'server-hardware error',
         })
-        self.oneview_client.server_profiles.get.side_effect = e
+        self.oneview_client.server_profiles.get_by_id.side_effect = e
 
         response = self.client.get(
             "/redfish/v1/Systems/b425802b-a6a5-4941-8885-aab68dfa2ee2/"
@@ -157,6 +160,6 @@ class TestNetworkInterface(BaseFlaskTest):
             response.status_code
         )
         self.assertEqual("application/json", response.mimetype)
-        self.oneview_client.server_profiles.get.assert_called_with(
+        self.oneview_client.server_profiles.get_by_id.assert_called_with(
             self.server_profile["uuid"])
-        self.oneview_client.server_hardware.get.assert_not_called()
+        self.oneview_client.server_hardware.get_by_uri.assert_not_called()
